c18249461c040ad91489011159718ed9248526b7
jpeterson0514 <>
2021-05-20 20:44:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// 1. Create a variable called dogName and set it to "Bixby"
+
+// Variable goes here ^^^
+console.log("This says 'Bixby':", dogName)
+//----------------------------------------
+
+// 2. Create a variable called dogAge and set it to 6
+
+// Variable goes here ^^^
+console.log("This says 6:", dogAge)
+//----------------------------------------
+
+// 3. Create an object variable called dog that has Bixby's age and name:
+
+// Object variable goes here ^^^
+console.log("This says 'Bixby':", dog.name)
+console.log("This says 6:", dog.age)
+//----------------------------------------
+
+// 4. Create a function called generateDog that generates a new dog when you give it a name and age:
+
+// Function goes here ^^^
+console.log("This says 'Mesa'", generateDog("Mesa", 3).name)
+console.log("This says 3", generateDog("Mesa", 3).age)
+//----------------------------------------
